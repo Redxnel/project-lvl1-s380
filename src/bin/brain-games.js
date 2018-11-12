e5b89e783ec87a3decc3faaf0e50d1724ad5f09b
redxnel <redxnel@yande.ru>
2018-11-12 19:35:59 +0300

    add question about name

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import { gamerName } from '..';
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!\n');
+
+console.log(`Hello, ${gamerName()}!`);
