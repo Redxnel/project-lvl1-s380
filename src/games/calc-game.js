f0cf223cc17763cc593917de1816994a0207b412
redxnel <redxnel@yandex.ru>
2018-11-15 21:31:46 +0300

    fix brain-calc game

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -4,36 +4,28 @@ import selectGame from '../tools/brain-games';
 const runGame = () => {
   const gameRule = 'What is the result of the expression?';
 
+  let result = 0;
+  const rightAnswer = () => result;
+
   const game = () => {
-    const operation = '+-*';
     const number1 = Math.floor(Math.random() * 100);
     const number2 = Math.floor(Math.random() * 100);
+    const operation = '+-*';
     const position = Math.floor(Math.random() * operation.length);
+    switch (operation[position]) {
+      case '+':
+        result = number1 + number2;
+        break;
+      case '-':
+        result = number1 - number2;
+        break;
+      default:
+        result = number1 * number2;
+        break;
+    }
     return `${number1} ${operation[position]} ${number2}`;
   };
 
-  const rightAnswer = (expression) => {
-    let number1 = '';
-    let number2 = '';
-    let operation = '';
-    for (let i = 0; i < expression.length; i += 1) {
-      if (expression[i + 1] === ' ' && expression[i - 1] === ' ') {
-        operation = expression[i];
-      } else if (expression[i] !== ' ' && i < expression.length / 2) {
-        number1 += expression[i];
-      } else if (expression[i] !== ' ') {
-        number2 += expression[i];
-      }
-    }
-    switch (operation) {
-      case '+':
-        return number1 + number2;
-      case '-':
-        return number1 - number2;
-      default:
-        return number1 * number2;
-    }
-  };
   selectGame(gameRule, game, rightAnswer);
 };
 
