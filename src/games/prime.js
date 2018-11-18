ddb6c0e139e825ec47eb466947b4e19b8cca1323
redxnel <redxnel@yandex.ru>
2018-11-18 13:04:11 +0300

    fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,21 +5,19 @@ import runGame from '../brain-games';
 
 const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const firstPrimeNumber = 2;
-const smallestDivisor = 2;
-
 const isPrime = (num) => {
+  const smallestDivisor = 2;
   const primeCheck = (digit, divisor) => {
     if (num === divisor) {
       return true;
     }
-    return num % divisor === 0 ? false : primeCheck(digit, divisor + 1);
+    return num % divisor === 0 || num <= 1 ? false : primeCheck(digit, divisor + 1);
   };
   return primeCheck(num, smallestDivisor);
 };
 
 const generateGameData = () => {
-  const number = randomDigit(100, firstPrimeNumber);
+  const number = randomDigit(100);
   const question = String(number);
   const rightAnswer = isPrime(number) ? 'yes' : 'no';
 
